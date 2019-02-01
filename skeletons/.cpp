c5e23a92c1cc2b07c5226a40c038d0a882edb122
ralismark <timmy.yao@outlook.com.au>
2019-02-01 15:41:39 +1100

    Add file skeletons

---
+++ skeletons/.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef L
+#define debug printf
+#else
+#define debug
+#endif
+
+
+
+int main()
+{
+#ifndef L
+	freopen("___in.txt", "r", stdin);
+	freopen("___out.txt", "w", stdout);
+#endif
+
+
+}
