de7be1f43cf6e31aa293b062d3617b3dfb402d44
ralismark <13449732+ralismark@users.noreply.github.com>
2024-01-21 21:24:38 +1100

    Sync 2024-01-21

--- init/10-plugin:vim-replacewithregister.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set("n", "s", "<Plug>ReplaceWithRegisterOperator")
-vim.keymap.set("n", "ss", "<Plug>ReplaceWithRegisterLine")
-vim.keymap.set("x", "s", "<Plug>ReplaceWithRegisterVisual")
